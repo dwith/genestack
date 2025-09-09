f06596dc77ed48f8a31dfaa7a381209a160214ef
dwith <dan.with@rackspace.com>
2025-09-09 13:38:43 -0500

    Cleanup: Adding scripts and doc changes for volume provisioning/qos
    Self-explanatory

--- scripts/create-volume-types-and-qos.sh
+++ scripts/create-volume-types-and-qos.sh
@@ -1,19 +1,3 @@
-# Set Volume Type Provisioning Specifications
-
-*Before* creating a volume within any volume type, the provisioning specifications must be set.
-
-## Minimum and Maximum volume size
-
-These specifications are set in the volume type. The following commands constrain the `lvmdriver-1` volume type to a size between 10 GB and 2 TB.
-
-``` shell
-root@openstack-node-0:~# kubectl --namespace openstack exec -ti openstack-admin-client -- openstack volume type set --property provisioning:min_vol_size=10 6af6ade2-53ca-4260-8b79-1ba2f208c91d
-root@openstack-node-0:~# kubectl --namespace openstack exec -ti openstack-admin-client -- openstack volume type set --property provisioning:max_vol_size=2048 6af6ade2-53ca-4260-8b79-1ba2f208c91d
-```
-
-## Sample Provisioning Script
-
-``` shell
 #!/bin/bash
 
 
@@ -106,4 +90,3 @@ openstack volume qos associate HA-Performance-Block HA-Performance
 ## Hide unsightly __DEFAULT__ from Skyline
 openstack volume type set --private __DEFAULT__
 
-```
